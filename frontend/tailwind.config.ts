1a32b029fc0f1e83823c8c666b994c3c6c67246d
estebangveltia <esteban.g@veltia.cl>
2025-07-26 17:15:22 -0400

    Add Next.js frontend skeleton

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('tailwindcss-animate'), require('@shadcn/ui/plugin')]
+}
+export default config
